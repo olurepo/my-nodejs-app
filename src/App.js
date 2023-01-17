237b5aa01e764099ed44e3db142cfb2ac09703d9
Olu <olu@pearloft.com>
2023-01-17 07:43:41 -0500

    add Dockerfile

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h2> Hello Andela! </h2>
+        {showText && <p>Looking forward to joining the pool.</p>}
+        <button
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={() => setShowText(true)}
         >
-          Learn React
-        </a>
+          Reveal
+        </button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//test('renders learn react link', () => {
+//  render(<App />);
+//  const linkElement = screen.getByText('Looking forward to joining the pool.';
+//  expect(linkElement).toBeInTheDocument();
+//});
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<App />);
+  
+  const button = screen.getByRole('button')
+  fireEvent.click(button)
+  expect(getByText('Looking forward to joining the pool.')).toBeInTheDocument();
+
 });
